4e9792a62fc3800f583329a0779cb2fc2c370a18
Denis Averin <59285247+Denis-Averin@users.noreply.github.com>
2020-07-01 15:18:35 +0700

    Ci (#7)
    * Load test configuration from environment
    
    * Use existing AccessToken when given
    
    * Create go.yml (#6)
    
    * Change base path in go.yml

--- test/configuration_test.go
+++ test/configuration_test.go
@@ -2,7 +2,10 @@ package test
 
 import (
 	"encoding/json"
+	"fmt"
 	"github.com/aspose-barcode-cloud/aspose-barcode-cloud-go/barcode/jwt"
+	"github.com/google/uuid"
+	"os"
 	"testing"
 
 	api "github.com/aspose-barcode-cloud/aspose-barcode-cloud-go/barcode"
@@ -49,7 +52,7 @@ func TestConfigMarshal(t *testing.T) {
 	bytes, err := json.Marshal(config)
 	require.Nil(t, err)
 
-	assert.Equal(t, "{\"jwt\":{\"clientId\":\"ClientID\",\"clientSecret\":\"ClientSecret\",\"tokenUrl\":\"TokenURL\"},\"api\":{\"basePath\":\"BasePath\",\"host\":\"Host\",\"userAgent\":\"UserAgent\"}}", string(bytes))
+	assert.Equal(t, "{\"jwt\":{\"clientId\":\"ClientID\",\"clientSecret\":\"ClientSecret\",\"tokenUrl\":\"TokenURL\",\"accessToken\":\"\"},\"api\":{\"basePath\":\"BasePath\",\"host\":\"Host\",\"userAgent\":\"UserAgent\"}}", string(bytes))
 }
 
 func TestNewConfigFromJson(t *testing.T) {
@@ -63,4 +66,39 @@ func TestNewConfigFromJson(t *testing.T) {
 	assert.Equal(t, "BasePath", config.APIConfig.BasePath)
 	assert.Equal(t, "Host", config.APIConfig.Host)
 	assert.Equal(t, "UserAgent", config.APIConfig.UserAgent)
+}
+
+func TestNewConfigFromEnvDefaults(t *testing.T) {
+	config, err := NewConfigFromEnv(uuid.New().String())
+	require.Nil(t, err)
+
+	assert.Equal(t, "", config.JwtConfig.ClientID)
+	assert.Equal(t, "", config.JwtConfig.ClientSecret)
+	assert.Equal(t, "https://api.aspose.cloud/connect/token", config.JwtConfig.TokenURL)
+
+	assert.Equal(t, "https://api.aspose.cloud/v3.0", config.APIConfig.BasePath)
+	assert.Equal(t, "", config.APIConfig.Host)
+	assert.Equal(t, "Aspose-Barcode-SDK/0.2006.1/go", config.APIConfig.UserAgent)
+}
+
+func TestNewConfigFromEnvValues(t *testing.T) {
+	uniqPrefix := uuid.New().String()
+	var err error
+	err = os.Setenv(fmt.Sprintf("%s_JWT_CLIENT_ID", uniqPrefix),
+		"jwt client id")
+	require.Nil(t, err)
+	err = os.Setenv(fmt.Sprintf("%s_JWT_CLIENT_SECRET", uniqPrefix),
+		"jwt client secret")
+	require.Nil(t, err)
+
+	config, err := NewConfigFromEnv(uniqPrefix)
+	require.Nil(t, err)
+
+	assert.Equal(t, "jwt client id", config.JwtConfig.ClientID)
+	assert.Equal(t, "jwt client secret", config.JwtConfig.ClientSecret)
+	assert.Equal(t, "https://api.aspose.cloud/connect/token", config.JwtConfig.TokenURL)
+
+	assert.Equal(t, "https://api.aspose.cloud/v3.0", config.APIConfig.BasePath)
+	assert.Equal(t, "", config.APIConfig.Host)
+	assert.Equal(t, "Aspose-Barcode-SDK/0.2006.1/go", config.APIConfig.UserAgent)
 }
