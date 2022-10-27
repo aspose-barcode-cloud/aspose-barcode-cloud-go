ce3a5481d6f8ca14dbca76b9a01bd34edaa57456
Ivan Kamkin <234-Ivan.Kamkin@users.noreply.git.saltov.dynabic.com>
2022-10-27 16:23:12 +0500

    Version up
    Pdf417MacroTerminator enum added

--- barcode/model_pdf417_params.go
+++ barcode/model_pdf417_params.go
@@ -70,4 +70,6 @@ type Pdf417Params struct {
 	MacroECIEncoding EciEncodings `json:"MacroECIEncoding,omitempty"`
 	// Function codeword for Code 128 emulation. Applied for MicroPDF417 only. Ignored for PDF417 and MacroPDF417 barcodes.
 	Code128Emulation Code128Emulation `json:"Code128Emulation,omitempty"`
+	// Used to tell the encoder whether to add Macro PDF417 Terminator (codeword 922) to the segment. Applied only for Macro PDF417.
+	Pdf417MacroTerminator Pdf417MacroTerminator `json:"Pdf417MacroTerminator,omitempty"`
 }
