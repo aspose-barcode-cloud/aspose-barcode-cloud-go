fa20817bf7a007e79dfcad4e11d9a5cce1056922
Ivan Kamkin <234-Ivan.Kamkin@users.noreply.git.saltov.dynabic.com>
2022-10-27 17:52:34 +0500

    Fix ver in test_config_test

--- test/test_config_test.go
+++ test/test_config_test.go
@@ -56,7 +56,7 @@ func TestNewConfigFromEnvDefaults(t *testing.T) {
 
 	assert.Equal(t, "https://api.aspose.cloud/v3.0", config.APIConfig.BasePath)
 	assert.Equal(t, "", config.APIConfig.Host)
-	assert.Equal(t, "Aspose-Barcode-SDK/0.2209.0/go", config.APIConfig.UserAgent)
+	assert.Equal(t, "Aspose-Barcode-SDK/0.2210.0/go", config.APIConfig.UserAgent)
 }
 
 func TestNewConfigFromEnvValues(t *testing.T) {
