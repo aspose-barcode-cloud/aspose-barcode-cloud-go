e36b466eb3f7fc141c3a4195de5f45a5c1491e54
Denis Averin <denis.averin@aspose.com>
2023-06-27 16:25:38 +0700

    Fixing tests

--- test/configuration_test.go
+++ test/configuration_test.go
@@ -15,7 +15,7 @@ func TestNewConfigurationBasePath(t *testing.T) {
 }
 
 func TestNewConfigurationUserAgent(t *testing.T) {
-	want := "Aspose-Barcode-SDK/0.2305.0/go"
+	want := "Aspose-Barcode-SDK/0.2306.0/go"
 	if got := api.NewConfiguration().UserAgent; got != want {
 		t.Errorf("NewConfiguration().UserAgent = %s; want %s", got, want)
 	}
