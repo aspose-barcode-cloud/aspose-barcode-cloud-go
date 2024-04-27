42321620345679bdf19f32a867654494af8119ab
Denis Averin <denis.averin@aspose.com>
2024-04-27 18:27:26 +0700

    Fix typo in docstring

--- barcode/model_australian_post_params.go
+++ barcode/model_australian_post_params.go
@@ -2,7 +2,7 @@ package barcode
 
 // AustralianPostParams - AustralianPost barcode parameters.
 type AustralianPostParams struct {
-	// Interpreting type for the Customer Information of AustralianPost, default to CustomerInformationInterpretingType.Other\"
+	// Interpreting type for the Customer Information of AustralianPost, default to CustomerInformationInterpretingType.Other
 	EncodingTable CustomerInformationInterpretingType `json:"EncodingTable,omitempty"`
 	// Short bar's height of AustralianPost barcode.
 	ShortBarHeight float64 `json:"ShortBarHeight,omitempty"`
