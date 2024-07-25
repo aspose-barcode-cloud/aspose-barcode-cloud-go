1722f3c212fd06b875b45bf98e88a05c8ab0657e
Denis Averin <denis.averin@aspose.com>
2024-07-25 15:13:42 +0700

    Bump version to 24.7

--- barcode/configuration.go
+++ barcode/configuration.go
@@ -54,7 +54,7 @@ func NewConfiguration() *Configuration {
 	cfg := &Configuration{
 		BasePath:      "https://api.aspose.cloud/v3.0",
 		DefaultHeader: make(map[string]string),
-		UserAgent:     "Aspose-Barcode-SDK/1.2406.0/go",
+		UserAgent:     "Aspose-Barcode-SDK/1.2407.0/go",
 	}
 	return cfg
 }
