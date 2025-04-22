1266548aaab21967b810e6ad66f6d4515ba7d10e
Ivan Kamkin <234-Ivan.Kamkin@users.noreply.git.saltov.dynabic.com>
2025-04-22 15:48:28 +0500

    Version updated
    Dependencies updated

--- barcode/client.go
+++ barcode/client.go
@@ -20,7 +20,7 @@ import (
 )
 
 const (
-	PACKAGE_VERSION         = "4.2503.0"
+	PACKAGE_VERSION         = "4.2504.0"
 	PACKAGE_NAME            = "go sdk"
 	X_ASPOSE_CLIENT         = "x-aspose-client"
 	X_ASPOSE_CLIENT_VERSION = "x-aspose-client-version"
