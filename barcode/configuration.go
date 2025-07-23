b90201b03a46ba5aaf7072729b0c6d5c8956c794
Ivan Kamkin <234-Ivan.Kamkin@users.noreply.git.saltov.dynabic.com>
2025-07-23 12:57:25 +0500

    Version updated

--- barcode/configuration.go
+++ barcode/configuration.go
@@ -54,7 +54,7 @@ func NewConfiguration() *Configuration {
 	cfg := &Configuration{
 		BasePath:      "https://api.aspose.cloud/v4.0",
 		DefaultHeader: make(map[string]string),
-		UserAgent:     "Aspose-Barcode-SDK/4.2506.0/go",
+		UserAgent:     "Aspose-Barcode-SDK/4.2507.0/go",
 	}
 	return cfg
 }
